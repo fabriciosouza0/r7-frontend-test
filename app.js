0678bc9aad3a679c01c6dbde0258dabcc79248ed
Fabricio <50778242+fabriciosouza0@users.noreply.github.com>
2025-02-12 11:40:10 -0300

    feat: allow cors

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-var app         = require('connect')()
-var serveStatic = require('serve-static')
+var app = require("connect")();
+var serveStatic = require("serve-static");
 
-app.use(serveStatic('public'))
+app.use(serveStatic("public"));
 
-console.log(' ➜   Open: http://localhost:7007')
-app.listen(7007)
+// Allow CORS, i don't need to add more complexity for that(K.I.S.S)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.listen(7007, () => {
+  console.log(" ➜   Open: http://localhost:7007");
+});
