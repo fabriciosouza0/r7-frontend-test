f5a5b6cb3e032ce5eab372c42325b33257205b16
Fabricio <50778242+fabriciosouza0@users.noreply.github.com>
2025-02-15 20:17:11 -0300

    refactor: specifying the attributes

--- public/javascripts/constants.js
+++ public/javascripts/constants.js
@@ -1,17 +1,5 @@
-/*
-  card width        = 360px
-  tooltip width     = 183.7px
-  image width       = 75px
-  card padding-left = 16px
-  gap               = 4px
-
-  the formula is equal to (360px - 183.7px - 75px - 16px - 4px => 81.3px) for medium/large devices(desktops)
-  the formula is equal to (300px - 183.7px - 75px - 16px - 4px => 21.3px) for mobile devices
-*/
 export const TOOLTIP_OFFSETS = {
-  DESKTOP: 81.3,
-  MOBILE: 21.3,
-  CARP_P_L: 16,
-  GAP: 4,
-  RIGHT_GAP: 24,
+  GAP: 4, // Space between the tooltip and the reference element
+  RIGHT_GAP: 16, // Right margin to prevent the tooltip from being too close to the container's edge
+  ARROW_SIZE: 16, // Size of the tooltip's arrow indicator
 };
